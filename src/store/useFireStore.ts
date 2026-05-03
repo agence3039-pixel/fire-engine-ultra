b8c07acdd2a26796ba46f42f792028a5a0d529cb
Rey <agence3039@gmail.com>
2026-05-03 23:10:49 +0200

    Fix: TypeScript verbatimModuleSyntax import

--- src/store/useFireStore.ts
+++ src/store/useFireStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
   import { persist, createJSONStorage } from 'zustand/middleware';
-  import { FireInputs } from '../utils/fireCalculations';
+  import type { FireInputs } from '../utils/fireCalculations';
 
   export type TransactionType = 'income' | 'expense';
 
